01ed4a03e64ea4741e6602c7cea3529798cddf91
malcalab <malcalab@everis.com>
2020-04-02 17:20:15 -0500

    Se realiza refactorizacion

---
+++ src/com/masivian/prueba/Constants.java
@@ -0,0 +1,13 @@
+package com.masivian.prueba;
+
+public interface Constants {
+    public static final int MAX = 1000;
+    public static final int RR=50;
+    public static final int CC=4;
+    public static final String TEXT1="The First ";
+    public static final String TEXT2=" Prime Numbers === Page ";
+    public static final String LINE="\n";
+    public static final String TAB="\f";
+    public static final String FORMAT="%10d";
+
+}
